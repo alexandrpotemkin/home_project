e1a279c13d145d2ad34503d03e755ca7d1454852
Alex Potemkin <koldunpot98@gmail.com>
2025-01-08 19:10:42 +0700

    Выполнена домашняя работа

--- src/file_reader.py
+++ src/file_reader.py
@@ -1,32 +1,47 @@
-from typing import List, Dict
+from typing import Dict, List
+
 import pandas as pd
 
 
 def read_transactions_from_csv(file_path: str) -> List[Dict[str, str]]:
     """
-    Считывает финансовые операции из CSV-файла.
+    Считывает транзакции из CSV-файла и возвращает их в виде списка словарей.
 
     :param file_path: Путь к CSV-файлу.
-    :return: Список словарей с транзакциями.
-    :raises ValueError: Если произошла ошибка при чтении файла.
+    :return: Список транзакций в формате словарей с ключами "date", "amount", "description".
     """
     try:
-        df = pd.read_csv(file_path)
-        return df.to_dict(orient='records')
+        data = pd.read_csv(file_path)
+        transactions = data.to_dict(orient="records")
+        return [
+            {
+                "date": str(transaction["date"]),
+                "amount": str(transaction["amount"]),
+                "description": str(transaction["description"]),
+            }
+            for transaction in transactions
+        ]
     except Exception as e:
         raise ValueError(f"Ошибка при чтении CSV-файла: {e}")
 
 
 def read_transactions_from_excel(file_path: str) -> List[Dict[str, str]]:
     """
-    Считывает финансовые операции из Excel-файла.
+    Считывает транзакции из Excel-файла и возвращает их в виде списка словарей.
 
     :param file_path: Путь к Excel-файлу.
-    :return: Список словарей с транзакциями.
-    :raises ValueError: Если произошла ошибка при чтении файла.
+    :return: Список транзакций в формате словарей с ключами "date", "amount", "description".
     """
     try:
-        df = pd.read_excel(file_path)
-        return df.to_dict(orient='records')
+        data = pd.read_excel(file_path)
+        transactions = data.to_dict(orient="records")
+        return [
+            {
+                "date": str(transaction["date"]),
+                "amount": str(transaction["amount"]),
+                "description": str(transaction["description"]),
+            }
+            for transaction in transactions
+        ]
     except Exception as e:
         raise ValueError(f"Ошибка при чтении Excel-файла: {e}")
